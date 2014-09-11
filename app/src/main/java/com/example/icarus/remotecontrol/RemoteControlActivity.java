949be29e0fffb23c6088ebdc49f379c6d88207c7
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-11 20:53:36 +0100

    RemoteControlActivity set-up

--- app/src/main/java/com/example/icarus/remotecontrol/RemoteControlActivity.java
+++ app/src/main/java/com/example/icarus/remotecontrol/RemoteControlActivity.java
@@ -1,25 +1,24 @@
 package com.example.icarus.remotecontrol;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class RemoteControlActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_remote_control);
+    protected Fragment createFragment() {
+        return new RemoteControlFragment();
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.remote_control, menu);
-        return true;
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
